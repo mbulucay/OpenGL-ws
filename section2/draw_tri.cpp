dfc0381936764310e5d89959464ce2d5fd40254b
mbulucay <ulucaybedir@gmail.com>
2022-08-03 13:06:56 +0300

    sonunda 3gen

--- section2/draw_tri.cpp
+++ section2/draw_tri.cpp
@@ -9,6 +9,53 @@
 // window size
 const GLuint width = 640, height = 480;
 
+static uint32_t ShaderProgram(const std::string& source, const uint32_t type){
+
+    uint32_t shaderId = glCreateShader(type);
+    // const char* src = &source[0];
+    const char* src = source.c_str();
+    glShaderSource(shaderId, 1, &src, nullptr);
+    glCompileShader(shaderId);
+
+    // error checking
+    int errorResult;
+    glGetShaderiv(shaderId, GL_COMPILE_STATUS, &errorResult);
+    if(errorResult == GL_FALSE){
+        int length;
+        glGetShaderiv(shaderId, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length);      // stack de dinamik olrak yer ayirmani sagliyor
+        glGetShaderInfoLog(shaderId, length, &length, message);
+        
+        std::cerr << "Failed to compile " << 
+            ((type == GL_VERTEX_SHADER) ? ("Vertex") : ("Fragment")) << " shader!" << std::endl;
+        std::cerr << message << std::endl;
+        glDeleteShader(shaderId);
+    }
+
+    return shaderId;
+}
+
+// sadece define ettigin file icerisnde cagirabiliyorusn
+static uint32_t CreateShader(const std::string& vertexShader, const std::string& fragmentShader){
+
+    uint32_t program = glCreateProgram();
+
+    uint32_t vertex_shader = ShaderProgram(vertexShader, GL_VERTEX_SHADER);
+    uint32_t fragment_shader = ShaderProgram(fragmentShader, GL_FRAGMENT_SHADER);
+
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+    return program;
+}
+
+
 int main(){
 
     // pencere yonetimi .... gibi seyler icin ilk bunu initialize etmemiz gerekiyor
@@ -56,6 +103,10 @@ int main(){
          0.5f, -0.5f 
     };
 
+    unsigned int vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
     uint32_t bufferId;
     glGenBuffers(1, &bufferId); // Gpu da yer alan bufferin id yani lokasyonu gibi bisey
     glBindBuffer(GL_ARRAY_BUFFER, bufferId); // Bufferin ne icin nasil kullanacagimizi berlirledigimiz yer
@@ -83,12 +134,51 @@ int main(){
 
     // glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+    // glsl verison 330 kullaniyoruz 
+    // code depraceted fonksiyon kullanmamiza izin yok
+    // In the process of authoring computer software, its standards or documentation, deprecation is a status applied to software features to indicate that they should be avoided, typically because they have been superseded. Although deprecated features remain in the software, their use may raise warning messages recommending alternative practices, and deprecation may indicate that the feature will be removed in the future. Features are deprecated—rather than immediately removed—in order to provide backward compatibility, and give programmers who have used the feature time to bring their code into compliance with the new standard.
+    // deprecation fonksiyon kullanmaktan kacinmamiz gereken fonksiyonlar bunlar ne olabilir eski surum, lack of portabiliy .... 
+    std::string vertex_shader = 
+        "#version 330 core\n"
+        "\n"
+        // atributelarin bulundugu index 0
+        "layout(location = 0) in vec4 position;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n";
+
+    const std::string vs = 
+        R"glsl(
+            #version 330 core
+
+            layout(location = 0) in vec4 position;
+
+            void main(){
+                gl_Position = position;
+            }
+        )glsl";
+
+    std::string fragment_shader = 
+        "#version 330 core\n"
+        "\n"
+        // atributelarin bulundugu index 0
+        "layout(location = 0) out vec4 color;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.0, 0.5, 0.3, 1.0);\n"
+        "}\n";
+
+    uint32_t shader = CreateShader(vs, fragment_shader);
+    glUseProgram(shader);
+
     while(!glfwWindowShouldClose(window)){
 
         glClear(GL_COLOR_BUFFER_BIT);
 
         glDrawArrays(GL_TRIANGLES, 0, 3);
-
         glfwSwapBuffers(window);
 
         glfwPollEvents();
